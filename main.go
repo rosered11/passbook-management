cf7aee1ba22b741863487db9042e013050391a2a
rosered <krihsana5@gmail.com>
2021-12-08 15:42:46 +0700

    set get ports from envar for heroku

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"log"
+	"os"
 	"rosered/passbook-management/src/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	app := fiber.New()
 
 	routes.Setup(app)
 
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
